e21ac328bc23714f8e4c1033287908e3a032865d
unknown <mtibbz@icloud.com>
2026-03-25 18:35:38 +0000

    Add viewer route logic

--- src/Application.tsx
+++ src/Application.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 
 import Layout from "./Layout.tsx";
 import Home from "./pages/Home.tsx";
+import Viewer from "./pages/Viewer.tsx";
 
 import "./assets/styles.css";
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route element={<Layout/>}>
           <Route path="/" element={<Home/>} />
+          <Route path="/viewer" element={<Viewer/>} />
           <Route path="*" element={<Navigate to="/" replace/>} />
         </Route>
       </Routes>
