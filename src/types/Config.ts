2952d30f60cda59399613f86f69f67a88f3d796d
unknown <mtibbz@icloud.com>
2026-03-31 16:10:16 +0100

    Why did i put commas?

--- src/types/Config.ts
+++ src/types/Config.ts
@@ -1,7 +1,7 @@
 export type Config = {
   API_URL: string;
-  DISCORD_SERVER_URL: string,
-  GAME_SERVER_CONNECT: string,
-  STEAM_GROUP_URL: string,
-  GITHUB_URL: string,
+  DISCORD_SERVER_URL: string;
+  GAME_SERVER_CONNECT: string;
+  STEAM_GROUP_URL: string;
+  GITHUB_URL: string;
 };
