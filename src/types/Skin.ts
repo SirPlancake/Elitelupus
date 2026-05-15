208d374184b9f07cec30b22c2ce7cad1a029e48a
SirPlancake <50420057+SirPlancake@users.noreply.github.com>
2026-05-15 03:28:13 +0100

    Add Admin tag

--- src/types/Skin.ts
+++ src/types/Skin.ts
@@ -13,14 +13,16 @@ export const SkinTypes: Record<SkinObject["type"], string> = {
   0: "Basic",
   1: "Management",
   2: "Unobtainable",
-  3: "Rebranded"
+  3: "Rebranded",
+  4: "Admin"
 };
 
 export const SkinColors: Record<SkinObject["type"], string> = {
   0: "text-gray-500",
   1: "text-lime-500",
   2: "text-orange-500",
-  3: "text-purple-500"
+  3: "text-purple-500",
+  4: "text-indigo-500"
 };
 
 export const SkinOrders: Record<number, number> = {
