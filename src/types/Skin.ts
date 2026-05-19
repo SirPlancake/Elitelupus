81f908eb9457eb79d2bc769f581a1b4aaba1271f
SirPlancake <50420057+SirPlancake@users.noreply.github.com>
2026-05-20 00:45:19 +0100

    Add and edit skin tags

--- src/types/Skin.ts
+++ src/types/Skin.ts
@@ -10,25 +10,34 @@ export type SkinObject = {
 };
 
 export const SkinTypes: Record<SkinObject["type"], string> = {
-  0: "Basic",
-  1: "Management",
-  2: "Unobtainable",
-  3: "Rebranded",
-  4: "Admin"
+  0: "Management",
+  1: "Admin",
+  2: "Unattainable",
+  3: "Epic",
+  4: "Rare",
+  5: "Uncommon",
+  6: "Common",
+  7: "Uncategorized",
 };
 
 export const SkinColors: Record<SkinObject["type"], string> = {
-  0: "text-gray-500",
-  1: "text-lime-500",
+  0: "text-lime-500",
+  1: "text-indigo-500",
   2: "text-orange-500",
   3: "text-purple-500",
-  4: "text-indigo-500"
+  4: "text-blue-500",
+  5: "text-green-500",
+  6: "text-gray-500",
+  7: "text-white-500",
 };
 
 export const SkinOrders: Record<number, number> = {
-  1: 0,
-  4: 1,
-  3: 2,
-  2: 3,
-  0: 4
+  0: 0,
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 6,
+  6: 6,
+  7: 7,
 };
